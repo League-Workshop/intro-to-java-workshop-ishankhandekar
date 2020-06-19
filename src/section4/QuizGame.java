053f7f577dbe7ff7614ea3370ccaf539927f232e
root <root@98b1d3da4b85>
2020-06-19 21:53:16 +0000

    Last Day

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -5,95 +5,170 @@ import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class QuizGame {
-	
+
+
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
 		int score = 0;
-		// 2.  Ask the user a question 
+		// 2.  Ask the user a question
+		
+		String question1 = null;
+		String question2 = null;
+		String question3 = null;
+		String question4 = null;
+		String question5 = null;
+		String question6 = null;
+		String question7 = null;
+		String question8 = null;
+		
+
+		for (int i = 0; i < 4; i++) {
+			
+		
 		Random randy = new Random();
-		int num1 = randy.nextInt(8);
-		int num2 = randy.nextInt(8);
-		int num3 = randy.nextInt(8);
-		int num4 = randy.nextInt(8);
-		if(num1 == 0) {
-			String question1 = JOptionPane.showInputDialog("True or False. Mammals don't lay eggs");
-		}
-		if(num2 == 1) {
-			String question2 = JOptionPane.showInputDialog("True or False. A triangle has four sides");
-			if(num2 == 2) {
-			String question3 = JOptionPane.showInputDialog("True or False. A howaiian pizza has pinapple toppings");
+		int randomNumber = randy.nextInt(8);
+		
+		System.out.println("Random number " + randomNumber );
+		
+		
+		
+		// joption -- ? will you win 1000000
+		if(randomNumber == 0) {
+			question1 = JOptionPane.showInputDialog("True or False. Mammals don't lay eggs");
+			
+			if (question1.equals("true")) {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score = score + 1;
+			}else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
 			}
-			if(num2 == 3) {
-				String question4 = JOptionPane.showInputDialog("True or False. Your liver pumps blood around your body");
+			
+		} else if(randomNumber == 1) {
+			question2 = JOptionPane.showInputDialog("True or False. A triangle has four sides");
+			if (question2.equals("true")) {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+				
+			}else {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score = score + 1;
 			}
-		String question1 = JOptionPane.showInputDialog("True or False. Mammals don't lay eggs");
-		String question2 = JOptionPane.showInputDialog("True or False. A triangle has four sides");
-		String question3 = JOptionPane.showInputDialog("True or False. A howaiian pizza has pinapple toppings");
-		String question4 = JOptionPane.showInputDialog("True or False. Your liver pumps blood around your body");
-		String question5 = JOptionPane.showInputDialog("True or False. White is a mixure of all colors");
-		String question6 = JOptionPane.showInputDialog("True or False. 10 x 10 = 100");
-		String question7 = JOptionPane.showInputDialog("True or False. snow white has 8 dwarfs");
-		String question8 = JOptionPane.showInputDialog("True or False. When there is a earthquake happens, the ground starts shaking");
-		// 3.  Use an if statement to check if their answer is correct
-		if (question1.equals("true")) {
-			JOptionPane.showMessageDialog(null, "Correct");
-			score = score + 1;
-		}else {
-			JOptionPane.showMessageDialog(null, "Incorrect");
+			
+			
+		} else if(randomNumber == 2) {
+			question3 = JOptionPane.showInputDialog("True or False. A howaiian pizza has pinapple toppings");
+			if (question3.equals("true")) {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score = score + 1;
+			}else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+			}
+			
+			
+		} else if(randomNumber == 3) {
+			question4 = JOptionPane.showInputDialog("True or False. Your liver pumps blood around your body");
+			if (question4.equals("true")) {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+			}else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+				score = score + 1;
+			}
+			
+		} else if(randomNumber == 4) {
+			question5 = JOptionPane.showInputDialog("True or False. White is a mixure of all colors");
+			if (question5.equals("true")) {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score = score + 1;
+			}else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+			}
+			
+		} else if(randomNumber == 5) {
+			question6 = JOptionPane.showInputDialog("True or False. 10 x 10 = 100");
+			if (question6.equals("true")) {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score = score + 1;
+			}else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+			}
+			
+		} else if(randomNumber == 6) {
+			question7 = JOptionPane.showInputDialog("True or False. snow white has 8 dwarfs");
+			if (question7.equals("true")) {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+			}else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+				score = score + 1;
+			}
+			
+		} else if(randomNumber == 7) {
+			question8 = JOptionPane.showInputDialog("True or False. When there is a earthquake happens, the ground starts shaking");
+			if (question8.equals("true")) {
+				JOptionPane.showMessageDialog(null, "Correct");
+				score = score + 1;
+			}else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+			}
 		}
-		//
-		if (question2.equals("true")) {
-			JOptionPane.showMessageDialog(null, "Incorrect");
-		}else {
-			JOptionPane.showMessageDialog(null, "Correct");
-			score = score + 1;
+		
+		
 		}
+		
+		JOptionPane.showMessageDialog(null, "congratulations, you got " + score + "/4!");
+			
+			
+		
+		
+		
+		
+				
+
+		
+		
+
+		
+//		
+//		
+//		if(num1 == 0) {
+//			question1 = JOptionPane.showInputDialog("True or False. Mammals don't lay eggs");
+//		}
+//		if(num2 >= 1 && num2 <= 8 ) {
+//			question2 = JOptionPane.showInputDialog("True or False. A triangle has four sides");
+//			if(num2 == 2) {
+//			question3 = JOptionPane.showInputDialog("True or False. A howaiian pizza has pinapple toppings");
+//			}
+//			if(num3 == 3) {
+//				question4 = JOptionPane.showInputDialog("True or False. Your liver pumps blood around your body");
+//			}
+//		}
+//		
+//		
+//		question5 = JOptionPane.showInputDialog("True or False. White is a mixure of all colors");
+//		question6 = JOptionPane.showInputDialog("True or False. 10 x 10 = 100");
+//		question7 = JOptionPane.showInputDialog("True or False. snow white has 8 dwarfs");
+//		question8 = JOptionPane.showInputDialog("True or False. When there is a earthquake happens, the ground starts shaking");
+//		// 3.  Use an if statement to check if their answer is correct
+
 		//
-		if (question3.equals("true")) {
-			JOptionPane.showMessageDialog(null, "Correct");
-			score = score + 1;
-		}else {
-			JOptionPane.showMessageDialog(null, "Incorrect");
-		}
+		
 		//
-		if (question4.equals("true")) {
-			JOptionPane.showMessageDialog(null, "Incorrect");
-		}else {
-			JOptionPane.showMessageDialog(null, "Incorrect");
-			score = score + 1;
-		}
+		
 		//
-		if (question5.equals("true")) {
-		JOptionPane.showMessageDialog(null, "Correct");
-		score = score + 1;
-	}else {
-		JOptionPane.showMessageDialog(null, "Incorrect");
-	}
+		
+		//
+		
 	//
-	if (question6.equals("true")) {
-		JOptionPane.showMessageDialog(null, "Correct");
-		score = score + 1;
-	}else {
-		JOptionPane.showMessageDialog(null, "Incorrect");
-	}
-	if (question7.equals("true")) {
-		JOptionPane.showMessageDialog(null, "Incorrect");
-	}else {
-		JOptionPane.showMessageDialog(null, "Incorrect");
-		score = score + 1;
-	}
-	if (question8.equals("true")) {
-		JOptionPane.showMessageDialog(null, "Correct");
-		score = score + 1;
-	}else {
-		JOptionPane.showMessageDialog(null, "Incorrect");
-	}
-		// 4.  if the user's answer was correct, add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
+	
+	
+	
+//		// 4.  if the user's answer was correct, add one to their score 
+//		
+//		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+//		
+//		// 6.  After all the questions have been asked, print the user's score 
+	
+	
+//	
 		
 	}
 }
--- src/section5/ScaryMaze.java
+++ src/section5/ScaryMaze.java
@@ -24,14 +24,15 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 
 	ScaryMaze() throws Exception {
 		//1. Use this online tool to make a maze image and drop it into your section5 package: https://www.pixilart.com/
-		//maze = ImageIO.read(getClass().getResource("standardMaze.jpg"));
+		maze = ImageIO.read(getClass().getResource("maze.png"));
 		//2. Change the line of code above so that it matches your maze's file name
 		
 		//3. Set the mouse pointer to the start of your maze using:
 		//new Robot().mouseMove(45, 95);
+		new Robot ().mouseMove(45, 95);
 		
 		//4. Add a mouse motion listener using:
-		//addMouseMotionListener(this);
+		addMouseMotionListener(this);
 		
 	}
 
@@ -40,14 +41,19 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseX = e.getX();
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
-		//5. Print the mouseColor variable 
+		//5. Print the mouseColor variable
+		System.out.println(mouseColor);
 		
-		//6.  Run your program and put your mouse over the background to find out what color it is
+		//6.  Run your program and put your mouse over the background to find out what color it
+		//is
 		
 		//7. Create an int variable that holds the background color. 
+		int backgroundColor = 16776961;
+
+	
 		
 		//8. If the mouse falls off the path (if it is on the background)
-	
+		
 				// call the scare method
 		
 		//13. If the mouse is on the end color
